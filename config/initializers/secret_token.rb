54d8fa3a321fffd94f3d46b555ccaeca74dc9562
Matthew Rock <mattrock@uwalumni.com>
2013-07-09 21:21:36 -0500

    Starting on chapter 8 - User Signup.
    Skipped the user picture thing at the end of Chapter 7.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'e77be55ba9e2a816da022f4373e35cc39221f36c4d15377379d36217c35d9590634df86accd9be84878c4024b1f97d510bab3cb7ef64208e317be847748d243f'
