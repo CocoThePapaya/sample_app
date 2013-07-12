6790cb6d83eb46139428bf5b29faaeeb785fb307
Matthew Rock <mattrock@uwalumni.com>
2013-07-11 21:32:10 -0500

    Finished Chapter 8

--- config/application.rb
+++ config/application.rb
@@ -59,5 +59,7 @@ module SampleApp
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+	
+	config.filter_parameters += [:password]
   end
 end
